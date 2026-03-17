0e9b60b764d4005fae664864d926c5414b253c96
Arman Gevorgyan <geworgiean@gmail.com>
2026-03-17 23:55:41 +0400

    added new color

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -56,7 +56,7 @@ export default function LoginPage() {
             <input 
               type="email" 
               placeholder="user@example.com" 
-              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition"
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-600 transition"
               onChange={(e) => setEmail(e.target.value)}
               required 
             />
