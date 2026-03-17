1e869e91d00cc7879ac6718db29bea0280718aaa
Arman Gevorgyan <geworgiean@gmail.com>
2026-03-17 23:09:53 +0400

    final

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,6 +11,15 @@ export default async function DashboardPage() {
 
     return (
         <div className="min-h-screen bg-gray-100 p-8 text-black">
+            <div className="p-8">
+                <h1 className="text-2xl font-bold">Անձնական Էջ
+                    {session?.user?.email && (
+                        <p className="mt-2 text-gray-600">
+                            Բարի գալուստ, <strong>{session.user.email}</strong>
+                        </p>
+                    )}
+                </h1>
+            </div>
             <div className="max-w-4xl mx-auto bg-white rounded-2xl shadow-lg p-10 border border-gray-100">
                 <div className="flex justify-between items-center border-b pb-6 mb-6">
                     <h1 className="text-3xl font-bold text-gray-800">Անձնական էջ</h1>
